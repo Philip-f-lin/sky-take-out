bc367fcdb7384aa66cbc49350e5d183d7a54c66b
linzhuofei <philip.f.lin@gmail.com>
2024-08-20 11:26:55 +0800

    新增菜品_代碼開發_1

--- sky-common/src/main/java/com/sky/utils/AliOssUtil.java
+++ sky-common/src/main/java/com/sky/utils/AliOssUtil.java
@@ -20,7 +20,7 @@ public class AliOssUtil {
     private String bucketName;
 
     /**
-     * 文件上传
+     * 文件上傳
      *
      * @param bytes
      * @param objectName
@@ -28,11 +28,11 @@ public class AliOssUtil {
      */
     public String upload(byte[] bytes, String objectName) {
 
-        // 创建OSSClient实例。
+        // 建立 OSSClient 實例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
         try {
-            // 创建PutObject请求。
+            // 建立 PutObject 請求。
             ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(bytes));
         } catch (OSSException oe) {
             System.out.println("Caught an OSSException, which means your request made it to OSS, "
@@ -52,7 +52,7 @@ public class AliOssUtil {
             }
         }
 
-        //文件访问路径规则 https://BucketName.Endpoint/ObjectName
+        //檔案存取路徑規則 https://BucketName.Endpoint/ObjectName
         StringBuilder stringBuilder = new StringBuilder("https://");
         stringBuilder
                 .append(bucketName)
@@ -61,7 +61,7 @@ public class AliOssUtil {
                 .append("/")
                 .append(objectName);
 
-        log.info("文件上传到:{}", stringBuilder.toString());
+        log.info("文件上傳到:{}", stringBuilder.toString());
 
         return stringBuilder.toString();
     }
