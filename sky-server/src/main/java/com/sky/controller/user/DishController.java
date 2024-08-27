2d97a13e7ff5bbba47a5d252e76c1d66022e6690
linzhuofei <philip.f.lin@gmail.com>
2024-08-27 15:06:16 +0800

    緩存菜品_代碼開發_緩存菜品數據

--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,9 +9,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController("userDishController")
@@ -19,8 +22,11 @@ import java.util.List;
 @Slf4j
 @Api(tags = "C端-菜色瀏覽接口")
 public class DishController {
-    @Autowired
+    @Resource
     private DishService dishService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     /**
      * 根據分類id查詢菜色
@@ -31,11 +37,23 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation("根據分類id查詢菜色")
     public Result<List<DishVO>> list(Long categoryId) {
+        // 構造 redis 中的 key，規則：dish_分類id
+        String key = "dish_" + categoryId;
+
+        // 查詢 redis 中是否存在菜色數據
+        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if (list != null && list.size() > 0){
+            // 如果存在，直接返回，無需查詢數據庫
+            return Result.success(list);
+        }
+
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);//查詢起售中的菜色
 
-        List<DishVO> list = dishService.listWithFlavor(dish);
+        // 如果不存在，查詢數據庫，將查詢到的數據存到 redis 中
+        list = dishService.listWithFlavor(dish);
+        redisTemplate.opsForValue().set(key, list);
 
         return Result.success(list);
     }
