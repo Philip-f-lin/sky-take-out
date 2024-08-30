20bfa548864f5ec3e66bc76a656c1ab080e64ee9
linzhuofei <philip.f.lin@gmail.com>
2024-08-30 17:13:48 +0800

    用戶下單_代碼開發2

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,13 +1,23 @@
 package com.sky.service.impl;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
+import com.sky.constant.MessageConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.AddressBook;
+import com.sky.entity.ShoppingCart;
+import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.ShoppingCartBusinessException;
+import com.sky.mapper.AddressBookMapper;
 import com.sky.mapper.OrderDetailMapper;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.ShoppingCartMapper;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderSubmitVO;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -18,6 +28,12 @@ public class OrderServiceImpl implements OrderService {
     @Resource
     private OrderDetailMapper orderDetailMapper;
 
+    @Resource
+    private AddressBookMapper addressBookMapper;
+
+    @Resource
+    private ShoppingCartMapper shoppingCartMapper;
+
     /**
      * 使用者下單
      * @param ordersSubmitDTO
@@ -25,6 +41,33 @@ public class OrderServiceImpl implements OrderService {
      */
     @Override
     public OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO) {
+        // 處理各種業務異常（地址簿為空，購物車數據為空）
+        AddressBook addressBook = addressBookMapper.getById(ordersSubmitDTO.getAddressBookId());
+        if(addressBook == null){
+            // 拋出業務異常
+            throw new AddressBookBusinessException(MessageConstant.ADDRESS_BOOK_IS_NULL);
+        }
+
+        // 查詢當前使用者的購物車數據
+        Long userId = BaseContext.getCurrentId();
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUserId(userId);
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
+
+        if (shoppingCartList == null || shoppingCartList.isEmpty()){
+            // 拋出業務異常
+            throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
+        }
+
+        // 向訂單表插入一條數據
+
+        // 向訂單明細表插入 n 條數據
+
+        // 清空當前使用者的購物車數據
+
+        // 封裝 VO 返回結果
+
         return null;
     }
 }
