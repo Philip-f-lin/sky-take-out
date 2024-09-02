f9ea2bb826245240b1696f002278537b5363dd98
linzhuofei <philip.f.lin@gmail.com>
2024-09-02 19:17:18 +0800

    營業額統計_代碼開發_3

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sky.service.impl;
 
+import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
@@ -9,8 +10,12 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -37,9 +42,27 @@ public class ReportServiceImpl implements ReportService {
             dateList.add(begin);
         }
 
+        // 存放每天的營業額
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate date : dateList) {
+            // 查詢 date 日期對應的營業額數據，營業額是指：狀態為"已完成"的訂單金額合計
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            // select * from orders where order_time > beginTime and order_time < endTime and status = 5
+            Map map = new HashMap<>();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = orderMapper.sumByMap(map);
+            turnoverList.add(turnover);
+        }
+
+        // 封裝返回結果
         return TurnoverReportVO
                 .builder()
                 .dateList(StringUtils.join(dateList, ","))
+                .turnoverList(StringUtils.join(turnoverList, ","))
                 .build();
     }
 }
